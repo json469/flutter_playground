764635e330b663fd33b8e9ea9976c9e1c1fad2a9
Jesse <json469@aucklanduni.ac.nz>
2018-12-07 14:42:01 +1300

    Add navigations and handle tap events

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/location.dart';
+import 'location_detail.dart';
 import 'styles.dart';
 
 class LocationList extends StatelessWidget {
@@ -14,14 +15,27 @@ class LocationList extends StatelessWidget {
         title: Text("Locations", style: Styles.navBarTitle)),
         body: ListView.builder(
           itemCount: this.locations.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              contentPadding: EdgeInsets.all(10.0),
-              leading: _itemThumbnail(this.locations[index]),
-              title: _itemTitle(this.locations[index]),
-            );
-          },
+          itemBuilder: _listViewItemBuilder,
         ),
+    );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int index) {
+    var location = this.locations[index];
+    return ListTile(
+      contentPadding: EdgeInsets.all(10.0),
+      leading: _itemThumbnail(location),
+      title: _itemTitle(location),
+      onTap: () => _navigateToLocationDetail(context, location)
+    );
+  }
+
+  void _navigateToLocationDetail(BuildContext context, Location location) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationDetail(location),
+      )
     );
   }
 
