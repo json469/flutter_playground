c4663b7444f7ea1902e429589684d2b04104d4b3
Jesse <json469.surface@users.noreply.github.com>
2018-12-10 16:39:59 +1300

    Add id in JSON and edit some testing

--- test/unit/location_test.dart
+++ test/unit/location_test.dart
@@ -1,24 +1,16 @@
 import '../../lib/models/location.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'dart:convert';
-
 
 void main() {
-  test('test location deserialization', () {
+  test('test /locations and /locations/:id', () async {
+    final locations = await Location.fetchAll();
+    for (var location in locations) {
+      expect(location.name, hasLength(greaterThan(0)));
+      expect(location.url, hasLength(greaterThan(0)));
 
-    const locationJSON =
-        '{ "name": "Arashiyama Bamboo Grove", "url": "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg", "facts": [{ "title": "Summary", "text": "This bamboo grove is on the outskirts of Kyoto." }] }';
-  
-    final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
-
-    expect("Arashiyama Bamboo Grove", equals(locationMap['name']));
-
-    final location = Location.fromJson(locationMap);
-
-    expect(location.name, equals(locationMap['name']));
-    expect(location.url, equals(locationMap['url']));
-
-    expect(location.facts[0].title, matches(locationMap['facts'][0]['title']));
-    expect(location.facts[0].text, matches(locationMap['facts'][0]['text']));
+      final fetchedLocation = await Location.fetchByID(location.id);
+      expect(fetchedLocation.name, equals(location.name));
+      expect(fetchedLocation.url, equals(location.url));
+    }
   });
-}
+} 
